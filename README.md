b15808b04cff9dda958ace8bb657ff82548d1b61
Luis Lorenzs <luislorenz7@gmail.com>
2025-01-16 16:56:43 +0100

    3

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# binary_search
+# principle
+- fast search 
+- slice a list alway in half 
+
+# plan
+- [ ] create a list of 100000 numbers
+- [ ] find one random number and record time 
+- [ ] record time with binary search 
+- [ ]
+- [ ] create a list of 1000 animal names in English 
+- [ ] search one excat animal with normal search and record time 
+- [ ]
+
---
+++ binary_search.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+# number list
+min = 1
+max = 100001
+numbers = list(range(min, max))
+
+# random_number
+random_num = random.choice(numbers)
+
+# start the record
+start_time = time.time()
+
+# standard search
+for x in numbers: 
+    if x == random_num:
+        # stop the record
+        end_time = time.time()
+
+        print('The number was found.')
+
+elapsed_time = end_time - start_time
+print(f"Execution time: {elapsed_time} seconds")
+
+# binary search
+half_max = (max - min) // 2 
+
+# test 2 
